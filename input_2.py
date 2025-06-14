f22d0ee4854c50179aa4bcc29da13fc1c442e6a0
ARJUN BHARATH <arjun200118bharath@gmail.com>
2025-06-14 22:51:29 +0530

    Add existing project files

---
+++ input_2.py
@@ -0,0 +1,31 @@
+# This code reads a file named "test.txt", replaces all occurrences of the word "java" with "Python",
+# and writes the modified content back to the same file.
+# # Uncomment the following lines to create a file named "test.txt" with some content
+
+# with open("test.txt", "w") as f:
+#     f.write("hi everyone\nwe are learning file handling in Python.\n")
+#     f.write("using java\ni like programming in java.\n")
+
+
+# changing java to Python in the file "test.txt"
+# Uncomment the following lines to create a file named "test.txt" with some content
+
+# with open("test.txt", "r") as f:
+#     data =f.read()
+#     data_new = data.replace("java", "Python")
+# with open("test.txt", "w") as f:
+#     f.write(data_new)
+
+#SEARCH WORD IN A FILE
+# Uncomment the following lines to create a file named "test.txt" with some content
+# with open("test.txt", "r") as f:
+#     data = f.read()
+#     print("enter the word you want to search in the file:")
+#     i = input()
+#     index = data.lower().find(i.lower())
+#     if index != -1:
+#         print(f"The word '{i}' is found in the file.")
+#     else:
+#         print(f"The word '{i}' is not found in the file.")
+
+#chech for line
---
+++ input_file.py
@@ -0,0 +1,41 @@
+#for this u need to first create a file named demo.txt with some content
+# and then read it using the code below
+# f=open("demo.txt","r")
+# data=f.read()
+# print(data)
+# f.close()
+
+# f=open("demo.txt","w")
+
+# f.write("This is a new line added to the file.\n")
+# f.write("This is another line.\n")  
+# f.close()
+# # Now reading the file again to see the changes     
+# f=open("demo.txt","r")
+# data=f.read()
+
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+# Now writing to the file
+with open("demo.txt", "w") as f:
+    f.write("This is a new line added to the file.\n")
+    f.write("This is anotherhhhhhhhhhhhhh line.\n")                          
+# Now reading the file again to see the changes
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+# Now appending to the file
+with open("demo.txt", "a") as f:
+    f.write("This is an appended line.\n")
+# Now reading the file again to see the changes
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+# Now deleting the file
+import os
+if os.path.exists("demo.txt"):
+    os.remove("demo.txt")
+    print("File deleted successfully")      
+else:
+    print("The file does not exist")                
